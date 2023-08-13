856b4868a242f43cbe5afd95756493c39a36fd21
BiHaiSong <haisong07@yeah.net>
2023-08-13 17:01:36 +0800

    Create new shell script getData.sh to collect data

---
+++ getData.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# usage:
+#   $1: 执行次数
+#   $2: iperf3 打流持续时间
+#   eg：bash getData.sh 5 1
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage:"
+    echo "\$1: Execution times"
+    echo "\$2: duration"
+    echo "Example: bash getData.sh 5 10"
+    echo "Collect traffic information 5 times, each lasting 10s"
+    exit 1
+fi
+
+# 指定调用脚本的次数
+total_calls=$1
+duration=$2
+
+# 循环调用脚本
+for ((i=1; i<=$total_calls; i++)); do
+    python send_traffic.py $duration
+    
+    sleepTime=$(( $duration + 10 ))
+    sleep $sleepTime
+done
